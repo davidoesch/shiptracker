66ff1fba03e6e2de8f04735959ea35a4d0615700
David Oesch <david.oesch@gmail.com>
2025-09-21 13:02:26 +0200

    Refactor AIS stream configuration; remove commented MMSI filter and update time threshold to 1 hour

--- config.py
+++ config.py
@@ -4,8 +4,9 @@
 # Ship MMSI filters - list of MMSI numbers to monitor
 # Example: ["244528000", "123456789", "987654321"]
 # REGINA MARIS is: "244528000"
+FILTERS_SHIP_MMSI = ["244528000"]
 # Blue Satr ferry is: "241087000"
-FILTERS_SHIP_MMSI = ["241087000"]
+#FILTERS_SHIP_MMSI = ["241087000"]
 
 # Maximum monitoring duration in minutes
 # How long to monitor the AIS stream before stopping
@@ -13,4 +14,4 @@ MAX_DURATION_MINUTES = 10
 
 # Time threshold in hours for duplicate entry filtering
 # Skip new entries if the latest entry is younger than this threshold
-TIME_THRESHOLD_HOURS = 0.1
+TIME_THRESHOLD_HOURS = 1
