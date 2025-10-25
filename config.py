fcfe9d652fd52d2a79022f30240c5f3b54d7995d
David Oesch <david.oesch@gmail.com>
2025-10-25 19:40:54 +0200

    Update ship MMSI filters and max durationa

--- config.py
+++ config.py
@@ -4,7 +4,8 @@
 # Ship MMSI filters - list of MMSI numbers to monitor
 # Example: ["244528000", "123456789", "987654321"]
 # REGINA MARIS is: "244528000"
-FILTERS_SHIP_MMSI = ["244528000"]
+FILTERS_SHIP_MMSI_ID = ["244528000","268855"]
+
 
 # Blue Star ferry is: "241087000"
 #FILTERS_SHIP_MMSI = ["241087000"]
@@ -14,7 +15,7 @@ FILTERS_SHIP_MMSI = ["244528000"]
 
 # Maximum monitoring duration in minutes
 # How long to monitor the AIS stream before stopping
-MAX_DURATION_MINUTES = 20
+MAX_DURATION_MINUTES = 12
 
 # Time threshold in hours for duplicate entry filtering
 # Skip new entries if the latest entry is younger than this threshold
